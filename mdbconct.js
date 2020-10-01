e5abc4b0eb89130813a6a47ff6873a765a67c62f
Maddi Harshavardhan <harshmaddi7@gmail.com>
2020-10-01 20:09:21 +0530

    secondcommit

--- mdbconct.js
+++ mdbconct.js
@@ -1,5 +1,10 @@
 var express= require('express');
+var bodyParser = require('body-parser');
+var middleware=require('./middleware');
+var server=require('./server');
 var app=express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const MongoClient=require('mongodb').MongoClient;
 const url='mongodb://127.0.0.1:27017';
 const dbName='mpone';//Type your database name which contains the collections of the hospital and ventilator names
@@ -9,63 +14,69 @@ let db
 MongoClient.connect(url, (err,client)=>{
     if(err) return console.log(err);
     db=client.db(dbName);
-app.get('/status', (req, res) => {
+app.get('/hospitalinfo',middleware.checkToken, (req, res) => {
+   db.collection(hosp).find().toArray().then(result => res.json(result));
+    });
+app.get('/ventilatorinfo',middleware.checkToken, (req, res) => {
+    db.collection(vent).find().toArray().then(result => res.json(result));
+    });
+app.post('/status',middleware.checkToken, (req, res) => {
 
     db.collection(vent, function (err, collection) {
-       // var x = String(req.query.cs);
-       collection.find({"status":req.query.cs}).toArray(function(err, items) {
+       // var x = String(req.body.cs);
+       collection.find({"status":req.body.cs}).toArray(function(err, items) {
         if(err) throw err;    
         res.send(items);            
     });
     
         });
     });
-    app.get('/hname', (req, res) => {
+    app.post('/hname',middleware.checkToken, (req, res) => {
 
         db.collection(vent, function (err, collection) {
-           // var x = String(req.query.cs);
-           collection.find({"name":req.query.hn}).toArray(function(err, items) {
+           // var x = String(req.body.cs);
+           collection.find({"name":req.body.hn}).toArray(function(err, items) {
             if(err) throw err;    
             res.send(items);            
         });
             });
         });
-        app.get('/hospname', (req, res) => {
+        app.post('/hospname',middleware.checkToken, (req, res) => {
 
             db.collection(hosp, function (err, collection) {
-               // var x = String(req.query.cs);
-               collection.find({"name":req.query.hn}).toArray(function(err, items) {
+               // var x = String(req.body.cs);
+               collection.find({"name":req.body.hn}).toArray(function(err, items) {
                 if(err) throw err;    
                 res.send(items);            
             });
                 });
             });  
 
-       app.put('/update',(req,res)=>{
+       app.put('/update',middleware.checkToken,(req,res)=>{
         db.collection(vent, function (err, collection) {
         
-            collection.update({"ventilatorId":req.query.vid}, { $set: {"status" : req.query.vcs} },
+            collection.update({"ventilatorId":req.body.vid}, { $set: {"status" : req.body.vcs} },
                     function(err, result){
                     if(err) throw err;    
                     console.log('Document Updated Successfully');
                     });
             });
         });
-        app.post('/add',(req,res)=>
+        app.post('/add',middleware.checkToken,(req,res)=>
         {
             db.collection(vent,function(err,collection)
             {
-                collection.insert({ "hId" :req.query.nid,
-                "ventilatorId" : req.query.nvid,
-                "status" : req.query.ns,
-                "name" : req.query.nn})
+                collection.insert({ "hId" :req.body.nid,
+                "ventilatorId" : req.body.nvid,
+                "status" : req.body.ns,
+                "name" : req.body.nn})
             });
         });
-        app.delete('/remove',(req,res)=>
+        app.delete('/remove',middleware.checkToken,(req,res)=>
         {
             db.collection(vent,function(err,collection)
             {
-                collection.remove({"ventilatorId":req.query.vid});
+                collection.remove({"ventilatorId":req.body.vid});
             });
         });
 
