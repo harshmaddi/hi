6fbec862adb21cc10f6ba61bb90dfb53324c68a6
harshmaddi <61815119+harshmaddi@users.noreply.github.com>
2020-10-03 13:37:08 +0530

    Update mdbconct.js

--- mdbconct.js
+++ mdbconct.js
@@ -7,32 +7,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const MongoClient=require('mongodb').MongoClient;
 const url='mongodb://127.0.0.1:27017';
-const dbName='mpone';//Type your database name which contains the collections of the hospital and ventilator names
-const vent ="ventoinfo";//Type your collection name of ventilator details
-const hosp='hospitalinfo';//Type your collection name of the hospital details
+//Type your database name which contains the collections of the hospital and ventilator names
+const dbName='mp1';
+//Type your collection name of ventilator details
+const vent ="vinfo";
+//Type your collection name of the hospital details
+const hosp='hinfo';
 let db
 MongoClient.connect(url, (err,client)=>{
     if(err) return console.log(err);
     db=client.db(dbName);
-app.get('/hospitalinfo',middleware.checkToken, (req, res) => {
-   db.collection(hosp).find().toArray().then(result => res.json(result));
+    //This gives the all hosp info
+    app.get('/hospitalinfo',middleware.checkToken, (req, res) => {
+        db.collection(hosp).find().toArray().then(result => res.json(result));
     });
-app.get('/ventilatorinfo',middleware.checkToken, (req, res) => {
-    db.collection(vent).find().toArray().then(result => res.json(result));
+    // All ventilator info
+    app.get('/ventilatorinfo',middleware.checkToken, (req, res) => {
+        db.collection(vent).find().toArray().then(result => res.json(result));
     });
-app.post('/status',middleware.checkToken, (req, res) => {
-
-    db.collection(vent, function (err, collection) {
+    //Gives the vent info by giving ventstatus
+    app.post('/status',middleware.checkToken, (req, res) => {
+        db.collection(vent, function (err, collection) {
        // var x = String(req.body.cs);
        collection.find({"status":req.body.cs}).toArray(function(err, items) {
         if(err) throw err;    
         res.send(items);            
     });
-    
-        });
     });
+    });
+    //Gives vent info when you give hosp name 
     app.post('/hname',middleware.checkToken, (req, res) => {
-
         db.collection(vent, function (err, collection) {
            // var x = String(req.body.cs);
            collection.find({"name":req.body.hn}).toArray(function(err, items) {
@@ -41,28 +45,22 @@ app.post('/status',middleware.checkToken, (req, res) => {
         });
             });
         });
-        app.post('/hospname',middleware.checkToken, (req, res) => {
-
-            db.collection(hosp, function (err, collection) {
-               // var x = String(req.body.cs);
-               collection.find({"name":req.body.hn}).toArray(function(err, items) {
-                if(err) throw err;    
-                res.send(items);            
-            });
-                });
-            });  
-
-       app.put('/update',middleware.checkToken,(req,res)=>{
-        db.collection(vent, function (err, collection) {
-        
-            collection.update({"ventilatorId":req.body.vid}, { $set: {"status" : req.body.vcs} },
-                    function(err, result){
-                    if(err) throw err;    
-                    console.log('Document Updated Successfully');
-                    });
-            });
+    //Gives hospital info when you give hosp name
+    app.post('/hospname',middleware.checkToken, (req, res) => {
+        db.collection(hosp, function (err, collection) {
+        // var x = String(req.body.cs);
+        collection.find({"name":req.body.hn}).toArray().then((result)=>res.send(result));
         });
-        app.post('/add',middleware.checkToken,(req,res)=>
+    });  
+    //Upadate the status of the vent by giving ventid 
+    app.put('/update',middleware.checkToken,(req,res)=>{
+        db.collection(vent, function (err, collection) {    
+        collection.update({"ventilatorId":req.body.vid},
+        { $set: {"status" : req.body.vcs} }).then((result)=>res.send(result));
+        });
+    });
+    //Adding a new ventilator
+    app.post('/add',middleware.checkToken,(req,res)=>
         {
             db.collection(vent,function(err,collection)
             {
@@ -71,14 +69,15 @@ app.post('/status',middleware.checkToken, (req, res) => {
                 "status" : req.body.ns,
                 "name" : req.body.nn})
             });
-        });
-        app.delete('/remove',middleware.checkToken,(req,res)=>
+    });
+    //Remove the vent by giving vent id
+    app.delete('/remove',middleware.checkToken,(req,res)=>
+    {
+        db.collection(vent,function(err,collection)
         {
-            db.collection(vent,function(err,collection)
-            {
-                collection.remove({"ventilatorId":req.body.vid});
-            });
+            collection.remove({"ventilatorId":req.body.vid});
         });
+    });
 
     app.listen(3000);
     
